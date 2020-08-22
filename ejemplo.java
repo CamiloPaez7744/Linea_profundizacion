dd4559dc8a7d40c0e65217a2df760ddc8c7e6dd3
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2020-08-22 15:48:06 -0500

    El primer Commit en area Local

---
+++ ejemplo.java
@@ -0,0 +1,15 @@
+package Linea_profundizacion;
+
+import java.util.Scanner;
+public class ejemplo{
+public static void main(String[]args){
+Scanner t=new Scanner(System.in);
+int n,n2,r;
+System.out.println("digite un numero1");
+n=t.nextInt();
+System.out.println("digite el numero 2");
+n2=t.nextInt();
+r=n+n2;
+System.out.println("suma="+r);
+}
+}
